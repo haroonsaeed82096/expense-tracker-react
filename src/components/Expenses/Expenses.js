964b7f775c251c3d8d59192c41981cb8bb110a1c
Haroon <haroon.saeed@outlook.com>
2021-12-12 17:24:22 +0100

    filter handler

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
@@ -7,10 +7,18 @@ import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
   return (
     <div>
       <Card className="expenses">
-        <ExpensesFilter></ExpensesFilter>
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
         <ExpenseItem
           title={props.items[0].title}
           amount={props.items[0].amount}
